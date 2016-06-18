fcb107071f95613490aa6c7d64c9f5e2ff2b11de
nkws <keigo_nakagawa@voyagegroup.com>
2016-06-18 15:24:51 +0900

    Initial Commit

---
+++ HelloSiri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloSiri
+//
+//  Created by 中川 慶悟 on 2016/06/18.
+//  Copyright © 2016年 中川 慶悟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HelloSiriTests/HelloSiriTests.swift
@@ -0,0 +1,36 @@
+//
+//  HelloSiriTests.swift
+//  HelloSiriTests
+//
+//  Created by 中川 慶悟 on 2016/06/18.
+//  Copyright © 2016年 中川 慶悟. All rights reserved.
+//
+
+import XCTest
+@testable import HelloSiri
+
+class HelloSiriTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
